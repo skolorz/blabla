e8a6458c6a14996975c35668a50dc7c2fda183f8
Szymon Kolorz <skolorz@sksi.pl>
2017-03-29 19:14:56 +0000

    wstepne generowanie

--- fillTransactions.js
+++ fillTransactions.js
@@ -3,8 +3,11 @@ const   moment = require("moment"),
 const   monthlyTransPatterns = require("./monthly-spendings.json"),
         startDate = moment("2015-05-01"),
         endDate = moment("2017-05-12");
-const   invPrefix = ["", "", "Customer no ", "Invoice ", "Bill number ", "Invoice "],
-        counterParties = { 
+const   
+        examples = { 
+            foreignLang:["English lessons", "", "język polski", "German lessons", "French lessons"],
+            invPrefix: ["", "", "Customer no ", "Invoice ", "Bill number ", "Invoice "],
+            firstName: ["Anna", "Jan", "Julia", "Stefan"],
             city: ["Bytom", "Katowice", "Sosnowiec", "Warszawa"],
             person: ["Jan Ptak", "Anna Korek", "Zbig Kowalski", "Mateusz Kosta"],
             street: ["ul. Prosta", "al. Jana Pawła II", "ul. Sokolska", "ul. Warszawska"],
@@ -12,7 +15,8 @@ const   invPrefix = ["", "", "Customer no ", "Invoice ", "Bill number ", "Invoic
             ElectricCompany: ["Tauron Energy", "Edison Electric Company", "Energa S.A.", "Tauron Energy"],
             insuranceCompany: ["Prudential Insurance", "WARTA S.A.", "PZU S.A.", "Allianz S.A."],
             satTv: ["ITI Neovision SAT", "Cyfrowy Polsat S.A.", "Vectra Cable TV S.A.", "SAT1 Inc"],
-            langSchool: ["Right Now Language School", "iProfi-Lingua sp. z o.o.", "#person, #randomAddr", "Sprachschule Berlin GmbH"],
+            langSchool: ["Right Now Language School", "iProfi-Lingua sp. z o.o.",
+                        "#person, #randomAddr", "Sprachschule Berlin GmbH"],
             gsmCompany: ["Orange S.A.", "Polkomtel GSM S.A.", "T-Mobile"],
             landlord: ["#person, #randomAddr", "#city Apartment Administration", 
                         "Spółdzielnia Mieszkaniowa Hutnik, #city"],
@@ -23,30 +27,31 @@ const   invPrefix = ["", "", "Customer no ", "Invoice ", "Bill number ", "Invoic
 
 var period = startDate.clone(),
     m,
+    generators = {},
     user = {city: "Katowice", name: "Adam Kroll" },
     patterns;
 
-function address(){
-    return random.pick(counterParties.street) 
+generators.baby = function (){
+    return random.pick(examples.firstName) + " " + user.name.match(/\w+ (\w+)/)[1];
+}
+generators.address = function(){
+    return random.pick(examples.street) 
             + " "
             + random.integer(1,90)
             + ", "
-            + random.pick(counterParties.city)
+            + random.pick(examples.city)
 }
-function invoice(){
-    return random.pick(invPrefix) 
+generators.inv = function(){
+    return random.pick(examples.invPrefix) 
             + "#long"
             + random.pick(["/",":","","-"])
             + random.pick(["#long",random.integer(10,1000),period.format("YYYY")]);
 }
+
 function replaceKeys(t){
-    var text = t;
-    if (!text) return t;
-    if (text.match(/#randomAddr/)){
-        text = text.replace("#randomAddr", address());
-    }
-    if (text.match(/#inv/)){
-        text = text.replace("#inv", invoice());
+    var m, text = t;
+    if (text.match(/#name/)){
+        text = text.replace("#name", user.name);
     }
     if (text.match(/#nr/)){
         text = text.replace("#nr", random.integer(1,50));
@@ -57,18 +62,24 @@ function replaceKeys(t){
     if (text.match(/#city/)){
         text = text.replace("#city",user.city );
     }
+    m = text.match(/#(\w+)/);
+    while (m && m[1] && examples[m[1]]) {
+        text = text.replace("#"+m[1], random.pick(examples[m[1]]));
+        m = text.match(/#(\w+)/);
+    }
+    while (m && m[1] && generators[m[1]]) {
+        text = text.replace("#"+m[1],generators[m[1]]);
+        m = text.match(/#(\w+)/);
+    }
     return text;
 }
 
 patterns = monthlyTransPatterns.filter(p => true || p.freq > random.integer(0,100));
 patterns = patterns.map(p => {
     p.description = replaceKeys(p.description);
-    m = p.counterpartyaccountholder.match(/#(\w+)/);
-    while (m && m[1] && counterParties[m[1]]) {
-        p.counterpartyaccountholder = random.pick(counterParties[m[1]])
-        m = p.counterpartyaccountholder.match(/#(\w+)/);
-    }
     p.counterpartyaccountholder = replaceKeys(p.counterpartyaccountholder);
+    p.counterpartyaccountholder = replaceKeys(p.counterpartyaccountholder);
+    delete p.field5;
     return p;
 });
 
@@ -78,6 +89,4 @@ while (period.isBefore(endDate)){
 }
 
 console.log(patterns);
-
-
 
